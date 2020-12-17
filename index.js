807043d1841edb22f01076fffb6d9b8d3f9e8a02
Gabriel Marcondes <gabriel.marcondes@zaig.com.br>
2020-12-17 19:27:21 -0300

    Adding "NumeroPrimo" function

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ exports.NumeroPar = function(n){
     }
     return resposta;
 }
+
+exports.NumeroPrimo = function(n){
+    var resposta = false;
+    // dividir o numero n vezes
+    for (i=1; i <= n; i++) {
+        if (n%1==0) {
+            count = count + 1;
+        }
+    }
+    if (count == 2) {
+        resposta = true;
+    }
+    return resposta;
+}
